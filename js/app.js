194a5545c7d716d700a61f7334064203eee7c02a
Jim Tang <jimtang@jimtang.com>
2013-10-27 10:25:16 +0000

    Added upvote

--- js/app.js
+++ js/app.js
@@ -1,22 +1,59 @@
 function PostCtrl($scope) {
-    var remoteLinks = new Firebase('https://improbanews.firebaseIO.com/posts/');
+    var FIREBASE_POSTS = 'https://improbanews.firebaseIO.com/posts/'
+    var remoteLinks = new Firebase(FIREBASE_POSTS);
     var firstTime = true;
+    $scope.posts=[]
 
-    remoteLinks.on('value', function(value) {
-        $scope.posts = [];
-        value.forEach(function(item) {
-            $scope.posts.push(item.val());
-        });
-        if(firstTime) {
-            $scope.$digest();
-            firstTime = false;
-        }
+    remoteLinks.on('child_added', function(childItem) {
+        var post = childItem.val();
+        post.ref = childItem.ref() + "";
+
+        $scope.posts.push(post);    
+        sortPosts();    
+        $scope.$digest();
     });
 
+    var sortPosts = function() {
+        $scope.posts.sort(function(postA, postB) {
+            // post priority = upvote - (age in days)
+            var postAgeA = new Date(postA.timestamp).getDay();
+            var postAgeB = new Date(postB.timestamp).getDay();
+            var postPriorityA = postA.upvote - postAgeA;
+            var postPriorityB = postB.upvote - postAgeB;
+            var result = postPriorityB - postPriorityA;
+            if (result == 0) {
+                return postB.timestamp - postA.timestamp;
+            }
+            return result;
+        });
+    }
+
     $scope.submitPost = function() {
-        $scope.post.timestamp = new Date().toLocaleString();
-        remoteLinks.push().setWithPriority($scope.post, -1.0 * new Date().getTime());
+        var date = new Date();
+        $scope.post.timestamp = date.getTime();
+        $scope.post.timeString = date.toLocaleString();
+        $scope.post.upvote = 1;
+        remoteLinks.push().setWithPriority($scope.post, -1.0 * date.getTime());
         $scope.post = {};
+    }
+
+    $scope.upvote = function(post) {
+        var remotePost = new Firebase(post.ref);
+        remotePost.child('upvote').set(post.upvote + 1);
+        alert("Vote received!")
+    }
+
+    $scope.upgradeRemotePostJson = function(post) {
+        var remotePost = new Firebase(post.ref);
+        alert(remotePost.child('upvote'));
+        var upgradedPost = []
+        upgradedPost.link = post.link;
+        upgradedPost.timeString = post.timestamp;
+        upgradedPost.timestamp = new Date().getTime();
+        upgradedPost.title = post.title;
+        upgradedPost.upvote = 1;
+        upgradedPost.user = post.user;
+        remotePost.set(upgradedPost);
     }
 }
 
